72439690379873c46f7d1d42774ed06f9f7c86f4
Benny Rice <benny.l.rice@gmail.com>
2021-01-30 00:58:50 -0700

    Figure 1B work

--- Figure 1.R
+++ Figure 1.R
@@ -4,6 +4,7 @@
 ######################################################################################
 
 library(tidyverse)
+library(here)
 
 ######################################################################################
 ### Figure 1A: Map with historical epidemics ###
@@ -15,31 +16,29 @@ library(tidyverse)
 ### 3: Maritime travel: Trans-atlantic slave trade: Africa - Americas
 ### 4: Air travel: SARS: Asia - Global
 
-### Mapping based off tutorial: https://www.r-graph-gallery.com/how-to-draw-connecting-routes-on-map-with-r-and-great-circles.html
+# Refs for classical antiquity pandemics
+# https://www.cambridge.org/core/journals/journal-of-economic-history/article/plague-and-lethal-epidemics-in-the-preindustrial-world/1D2D564AD8560ABACAF9D81A65F27CED
+# https://www.sciencedirect.com/science/article/pii/S1473309913703232?casa_token=sbNL1Stx6PoAAAAA:coZNre9d3_OrnynZmAmaAiHEVoK_b_yb-LB7G_kJjFvvgNXKdWCFiNTNnUkgoD2hwibNGU7sCA
+# https://www.cambridge.org/core/journals/journal-of-roman-archaeology/article/abs/impact-of-the-antonine-plague/B510B697DB444B274465A50381267380
+# https://www.cambridge.org/core/journals/journal-of-roman-archaeology/article/abs/pandemics-and-passages-to-late-antiquity-rethinking-the-plague-of-c249270-described-by-cyprian/BF0A142F1864D05FDC0C3439D182B7B0
+
+# Ref for Trans-atlantic slave trade and malaria:
+# https://www.annualreviews.org/doi/abs/10.1146/annurev-genet-120215-035211?casa_token=B9GHXaSi0ZIAAAAA:I8lTQkLr6DGzd39CSCylKr-jDMQ8mfnS2jrv6CwtV38mre5tadEUwxpFsGeVfYp8iOv4qGRV1bHX
 
 # Generating a map of the world, removing Antarctica (author is unaware of any epidemics in Antarctica)
 map.world <- map_data('world') %>% filter(region != "Antarctica")
 
 # Plotting map
 ggplot(map.world, aes(x = long, y = lat, group = group)) +
-  geom_polygon(fill="#a1887f", colour = "white", size = 0.01) +
+  geom_polygon(fill="#d7ccc8", colour = "white", size = 0.04) +
   coord_fixed(1.3) +
-  theme(panel.background = element_blank())
+  theme(panel.background = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank())
 
-# 
+# Adding arrows for movements in powerpoint (could do in R but would take more time)
 
-
-
-# remove antartica, remove background, make nice color map, country boundaries slight, 
-# color coded arrows from manually entered coordinates, follow hartl map style, color legend: air travel, overland, maritime
-# arrows: overland from eastern med to western med; africa to americas per hartl; europe to americas;
-# SARS arrows: trickier; link stepwise across eurasia to get to americas
-
-# Refs
-# https://www.cambridge.org/core/journals/journal-of-economic-history/article/plague-and-lethal-epidemics-in-the-preindustrial-world/1D2D564AD8560ABACAF9D81A65F27CED
-# https://www.sciencedirect.com/science/article/pii/S1473309913703232?casa_token=sbNL1Stx6PoAAAAA:coZNre9d3_OrnynZmAmaAiHEVoK_b_yb-LB7G_kJjFvvgNXKdWCFiNTNnUkgoD2hwibNGU7sCA
-# https://www.cambridge.org/core/journals/journal-of-roman-archaeology/article/abs/impact-of-the-antonine-plague/B510B697DB444B274465A50381267380
-# https://www.cambridge.org/core/journals/journal-of-roman-archaeology/article/abs/pandemics-and-passages-to-late-antiquity-rethinking-the-plague-of-c249270-described-by-cyprian/BF0A142F1864D05FDC0C3439D182B7B0
 
 
 
@@ -50,10 +49,18 @@ ggplot(map.world, aes(x = long, y = lat, group = group)) +
 # 'more frequent transport of people and goods between larger, denser population centers'
 
 #Grab, clean worldbank data
-#Split by region? 3 rows x 2 columns
-# East Asia & Pacific; Europe & Central Asia; Latin America & Caribbean; Middle East & North Africa; Sub-Saharan Africa
-# Needs US and Canada?
-# Check for the 3 letter codes to grab these regions
+# Show world above then split by region? 3 rows x 2 columns
+# WLD World
+
+# EAS East Asia & Pacific
+# ECS Europe & Central Asia
+# MEA Middle East & North Africa 
+# SSF Sub-Saharan Africa
+# LCN Latin America & Caribbean
+# NAC North America
+
+
+
 
 
 
